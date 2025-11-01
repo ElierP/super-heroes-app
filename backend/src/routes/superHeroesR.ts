78df5973441ac780009b7e41b953193b38da30a4
ElierP <elier.efrenpina@gmail.com>
2025-10-31 19:15:31 -0600

    [DEV] React with Front home page

---
+++ backend/src/routes/superHeroesR.ts
@@ -0,0 +1,10 @@
+import express from "express"; 
+import { getHeroes, getHeroById, getHeroByName } from "../controllers/superHeroesC"; 
+
+const router = express.Router();
+
+router.get("/", getHeroes);
+router.get("/id/:id", getHeroById);
+router.get("/name/:name", getHeroByName);
+
+export default router;
